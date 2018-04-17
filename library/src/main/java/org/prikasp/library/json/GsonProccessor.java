df7a3da3930b7bd567abc3dafd3219605b7f30a3
libre <libre@librePC>
2018-04-18 00:55:01 +0300

    Json parser implemented

---
+++ library/src/main/java/org/prikasp/library/json/GsonProccessor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.prikasp.library.json;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.LinkedList;
+import java.util.List;
+import org.prikasp.library.enteties.Author;
+import org.prikasp.library.enteties.Book;
+
+/**
+ *
+ * @author libre
+ */
+public class GsonProccessor 
+{
+    static Gson proccessor = new Gson();
+    
+    public static Book parseBook(String jsonBook)
+    {
+        JsonObject jsonObject = new JsonParser().parse(jsonBook).getAsJsonObject();
+        String title = jsonObject.get("title").getAsString();
+        int year = jsonObject.get("year").getAsInt();
+        List<Author> authors = new LinkedList<>();
+        JsonArray arr = jsonObject.get("authors").getAsJsonArray();
+        for (JsonElement jsonElement : arr) 
+        {
+            authors.add(new Author(jsonElement.getAsString()));
+        }
+        return new Book(title, year, authors);
+    }
+}
