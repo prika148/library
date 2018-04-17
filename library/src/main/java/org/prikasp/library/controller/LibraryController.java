0990acfc67d58ea3ec50a1325c6600ebd59b21d1
libre <libre@librePC>
2018-04-18 00:56:59 +0300

    Create task implemented in Controller

--- library/src/main/java/org/prikasp/library/controller/LibraryController.java
+++ library/src/main/java/org/prikasp/library/controller/LibraryController.java
@@ -6,6 +6,9 @@
 package org.prikasp.library.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import org.prikasp.library.database.SimpleDataBaseWorker;
+import org.prikasp.library.enteties.Book;
+import org.prikasp.library.json.GsonProccessor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -22,12 +25,27 @@ public class LibraryController
     {
         return "index";
     }
-    
+        
     @RequestMapping(value = "/add_book.htm")
     public String addBook(HttpServletRequest request,  ModelMap model)
     {
-        System.out.println(request.getParameter("json"));
-        model.addAttribute("text", "Успешно добавлено");
+        String clientInput = request.getParameter("json");
+        String clientOut = null;
+        
+        Book parsed = null;
+        try
+        {
+            parsed = GsonProccessor.parseBook(clientInput);
+            if(SimpleDataBaseWorker.getWorker().saveBook(parsed))
+                clientOut = "Успешно добавлено!";
+        }
+        catch (NumberFormatException ex)
+        {
+            clientOut = "Неверный формат года издания. Используйте только цифры";
+        }
+        if(clientOut == null)
+            clientOut = "При добалении произошла ошибка";
+        model.addAttribute("text", clientOut);
         return "response";
     }
 }
